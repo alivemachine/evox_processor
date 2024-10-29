de51b64612ba9137cfac002f773cf0aeaf440da8
alivemachine <42188925+alivemachine@users.noreply.github.com>
2024-10-29 12:00:04 -0600

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -5,7 +5,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'evox_processor_storage',
   access: (allow) => ({
-    '/*': [
+    '*/*': [
       allow.authenticated.to(['read','write']),
       allow.guest.to(['read', 'write'])
     ],
