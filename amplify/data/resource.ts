0b53a7e78f86070a369b0dea18fa54f26b3c6532
alivemachine <42188925+alivemachine@users.noreply.github.com>
2024-10-29 13:27:07 -0600

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,15 +1,17 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 /*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
+The section below creates a database with the structure {"id#":{"color":{"angle":{}}}}.
+The authorization rule below specifies that any user authenticated via an API key 
+can "create", "read", "update", and "delete" any records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Item: a
     .model({
-      content: a.string(),
+      id: a.string(),
+      color: a.map(a.model({
+        angle: a.map(a.model({}))
+      }))
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
